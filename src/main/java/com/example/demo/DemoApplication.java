e597c45966caad388f026c2ec3ce7e060999637e
Yuta Yukawa <ykwyuta@gmail.com>
2023-04-17 13:29:10 +0000

    test用のエンドポイントを追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
+
+	@GetMapping("/slow")
+	public String slow(int sleep) {
+		try {
+			Thread.sleep(sleep * 1000);
+		} catch (InterruptedException e) {
+			return e.getMessage();
+		}
+		return "sleep";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
